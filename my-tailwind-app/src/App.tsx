ac028c27c4a1d21ae3b6b26f2500534a7e9f424f
Sahil Tuladhar <sailratna11@gmail.com>
2025-07-22 06:58:02 +0545

    pages folder added - FE

--- my-tailwind-app/src/App.tsx
+++ my-tailwind-app/src/App.tsx
@@ -1,44 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import { CustomerReviews , Footer , Hero , Navbar, PopularProducts , Services , SpecialOffer , Subscribe , SuperQuality } from './files';
-
+import HomePage from './pages/HomePage';
 import Hello from './CrashCourse/Hello';
 
 function App() {
   return (
-    <main className='relative '>
-      <Navbar/>
-      <section className='xl: padding-l wide:padding-r padding-b max-w-full'>
-        <Hero />
-      </section>
-      <section className='padding'>
-        <PopularProducts />
-      </section>
 
-      <section className='padding flex justify-center items-center'>
-        <SuperQuality/>
-      </section>
-
-      <section className='padding-x py-10'>
-        <Services/>
-      </section>
-
-      <section className='padding'>
-        <SpecialOffer/>
-      </section>
-      
-      <section className='bg-pale-blue padding'>
-      <CustomerReviews/>
-      </section>
-
-      <section className='padding-x sm:py-32 py-16 w-full'>
-        <Subscribe/>
-      </section>
-
-      <section className='bg-black padding-x padding-t pb-8'>
-        <Footer/>
-      </section>
-    </main>
+    <> 
+       <HomePage />
+    </>
+    
   );
 }
 
